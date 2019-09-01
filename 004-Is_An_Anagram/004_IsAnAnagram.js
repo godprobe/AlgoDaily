edcd8254ba90008a441fe15bc97d4f43dcf84f35
godprobe <godprobe.misc+git@gmail.com>
2019-08-31 21:47:12 -0700

    Fixed letter case issue with Day 4

--- 004-Is_An_Anagram/004_IsAnAnagram.js
+++ 004-Is_An_Anagram/004_IsAnAnagram.js
@@ -3,10 +3,11 @@ function isAnagram(str1, str2) {
 	// Input: str1 and str2 are strings to be compared
 	// Return: true or false, depending on whether or not inputs are anagrams
 	// TODO: test if running a for loop on the sorted array reduces overhead
-	return str1.split('').sort().toString() == str2.split('').sort().toString();
+	// TODO: check official solution
+	return str1.toLowerCase().split('').sort().toString() == str2.toLowerCase().split('').sort().toString();
 }
 
-console.log(isAnagram("cinema", "iceman"));
+console.log(isAnagram("CiNeMa", "ICEman"));
 console.log(isAnagram("break", "beaker"));
 console.log(isAnagram("",""));
 console.log(isAnagram("wave","wav"));

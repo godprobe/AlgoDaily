7a718980b19e90e3b39bfa13e3551d8f1005ee8f
godprobe <godprobe.misc+git@gmail.com>
2019-09-02 13:55:39 -0700

    Day 8 solved, but possibly not in the time complexity constraint.

---
+++ 008-Lonely_Number/008_LonelyNumber.js
@@ -0,0 +1,24 @@
+function lonelyNumber(nums) {
+	// Description: Returns the only number for which there is a single instance in an array of numbers, in O(n) linear time.
+	// Input: An array of numbers, with one number guaranteed to be unique.
+	// Returns: The only unique number in the array.
+	
+	for (let i = 0; i < nums.length; i++) {
+		// Drop the element and includes() or indexOf(), but prob not O(n)...
+		check = nums.shift();
+		if (nums.indexOf(check) == -1) {
+			return check;
+		} else {
+			// Stitch checked elements back on, to prevent creating wrong uniques
+			nums.push(check);
+		}
+	}
+	return "flew right past...";
+}
+
+console.log(lonelyNumber([0]));
+console.log(lonelyNumber([0,0,1]));
+console.log(lonelyNumber([2,1,0,0,1]));
+console.log(lonelyNumber([123,23,12,3,123,23,12,1,1,2,2]));
+console.log(lonelyNumber([1,2,3,1,2,3,4]));
+console.log(lonelyNumber([12,23,34,45,56,67,12,23,45,56,67]));
